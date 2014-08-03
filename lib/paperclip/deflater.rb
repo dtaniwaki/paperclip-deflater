abb1c41d3d90e3a84037b1b55614700db4a86dc3
dtaniwaki <daisuketaniwaki@gmail.com>
2014-08-03 19:29:12 +0900

    Add plain processor

--- lib/paperclip/deflater.rb
+++ lib/paperclip/deflater.rb
@@ -1,8 +1,10 @@
 require 'paperclip'
+require 'paperclip/processors/plain'
 require 'paperclip/processors/deflater'
 require 'paperclip/processors/gzip'
 
 Paperclip.configure do |c|
+  c.register_processor :plain, Paperclip::Processors::Plain
   c.register_processor :deflater, Paperclip::Processors::Deflater
   c.register_processor :gzip, Paperclip::Processors::Gzip
 end
